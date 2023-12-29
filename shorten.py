3ce8b830d11d032cfba5905e0ad73e474597f0c5
Femi Alamu <femialamu@hotmail.com>
2023-12-28 23:51:17 -0500

    shorten

--- shorten.py
+++ shorten.py
@@ -1,12 +1,79 @@
-
+import string
+import random
+import sqlite3
 
 def shortener(url, alias=None):
     '''
     enter a url and return the shortened url
     '''
-    if alias: 
-        shortened_url = None # add logic ot shorten the url here
-    else:
-        shortened_url = None # 
 
-    return shortened_url 
+    def _shorten(url, alias=None):
+        
+        if alias:
+            shortened_url = alias
+            
+        else: 
+            # generate 8-10 word alphanumeric string based on randomly selected numbers
+            alphabet = string.ascii_letters + string.digits
+            chars = len(alphabet) - 1
+            DEFAULT_LENGTH = random.randint(8,10)
+            encode = []
+            for _ in range(DEFAULT_LENGTH):
+                num = random.randint(0, chars)
+                encode.append(num)
+            
+            shortened_url = ''.join(alphabet[char] for char in encode)
+        update_stmt = f"insert into url_shorten(base_url, short_url) values ('{url}', '{shortened_url}');"
+        
+        try:
+            with sqlite3.connect('url.db') as conn:
+                cur = conn.cursor()
+                cur.execute(update_stmt)
+                conn.commit()
+        
+        except Exception as e:
+            print(e)
+            _shorten(url)
+        return shortened_url
+
+    check_shortened_urls(url, check_type='base')
+
+    if alias: 
+        check_shortened_urls(alias, check_type = 'alias')
+        shortened_url = _shorten(url, alias) # add logic to shorten the url here
+
+    else:
+        shortened_url = _shorten(url) # add logic to shorten the url 
+
+    return shortened_url
+
+
+def check_shortened_urls(url, check_type):
+    ''' 
+    lists all the urls that have been shortened 
+    '''
+    table = 'url_shorten'
+    
+    if check_type == 'base':
+        field = 'base_url'
+    elif check_type == 'alias': 
+        field = 'short_url'
+    else:
+        raise TypeError('check type must be either `url` or `alias`')
+
+    print(url)
+    with sqlite3.connect('url.db') as conn:
+        check_url_stmt = f"SELECT * FROM {table} WHERE {field} = '{url}'"
+        cur = conn.cursor()
+        response = cur.execute(check_url_stmt)
+        result = response.fetchone()
+    
+    if result is None:
+        return 
+    else: 
+        raise Exception(f'{field} already exists')
+
+# shortener('amazon.ca', alias='amazon')
+# shortener('google.com', alias='google')
+shortener('doordash.ca',)
+
